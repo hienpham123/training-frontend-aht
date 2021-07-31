3c1039ccf822ddacc5c8bbb6cadb82c1ac31d679
Kim Van Ha <kimvanha01@gmail.com>
2021-07-31 17:34:08 +0700

    fix bug

--- assets/js/tab.js
+++ assets/js/tab.js
@@ -1,15 +1,14 @@
-function openProduct(evt, cityName) {
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-      tabcontent[i].style.display = "none";
-    }
-    tablinks = document.getElementsByClassName("tablinks");
-    for (i = 0; i < tablinks.length; i++) {
-      tablinks[i].className = tablinks[i].className.replace(" active", "");
-    }
-    document.getElementById(cityName).style.display = "block";
-    evt.currentTarget.className += " active";
+function openProduct(event, productName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
   }
-    document.getElementById("defaultOpen").click();
-  
+  tablinks = document.getElementsByClassName("tab--links");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(productName).style.display = "block";
+  event.currentTarget.className += " active";
+}
+document.getElementById("defaultOpen").click();
